c598e4f1efefb5aaa5935734c921e4aebd725e92
naomyscha <naomyschaam@gmail.com>
2025-11-12 17:19:29 +0700

    tutorial 8 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naomyscha-attalie-footballnews.pbp.cs.ui.ac.id",]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "naomyscha-attalie-footballnews.pbp.cs.ui.ac.id","10.0.2.2",]
 CSRF_TRUSTED_ORIGINS = ["https://naomyscha-attalie-footballnews.pbp.cs.ui.ac.id",]
 
 
@@ -45,10 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'corsheaders',
+    'authentication',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +60,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
 
 ROOT_URLCONF = 'football_news.urls'
 
--- football_news/urls.py
+++ football_news/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('main.urls', 'main'), namespace='main'))
+    path('', include(('main.urls', 'main'), namespace='main')),
+    path('auth/', include('authentication.urls')),
 ]
